fc1c699bf95d38c0b23297a668d563b7b10f0ed4
Jinhyunly <dbdbddipp@gmail.com>
2018-05-23 22:58:01 +0900

    add dummy model

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,34 @@
+import SearchModel from './models/SearchModel.js'
 new Vue({
     el: '#app',
     data: {
-        query: '' //입력데이터를 받아서 저장한다
+        query: '', //입력데이터를 받아서 저장한다
+        submitted: false,
+        searchResult: []
     },
     methods: {
         onSubmit(e){
-            debugger
+            // debugger
+            this.search()
         },
-        onKeyup(){
+        onKeyup(e){
             if(!this.query.length) this.onReset()
         },
-        onReset(){
+        onReset(e){
+           this.resetForm()
+        },
+        search(){
+            SearchModel.list().then(data => {
+                this.submitted = true
+                this.searchResult = data
+            })
+        },
+        resetForm(){
             this.query= '' //this 는 vue인스턴스를 의미
+            this.submitted = false
+            this.searchResult = []
             //todo 검색결과를 숨기는 ...
-            debugger
+            // debugger
         }
         
     }
