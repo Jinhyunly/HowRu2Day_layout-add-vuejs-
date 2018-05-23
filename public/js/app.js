718b3aa20a12712481f075832484e0372726adc0
Jinhyunly <dbdbddipp@gmail.com>
2018-05-23 17:55:21 +0900

    add cummunity search bind vuejs

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,20 @@
 new Vue({
     el: '#app',
     data: {
-        msg: 'hello wold'
+        query: '' //입력데이터를 받아서 저장한다
+    },
+    methods: {
+        onSubmit(e){
+            debugger
+        },
+        onKeyup(){
+            if(!this.query.length) this.onReset()
+        },
+        onReset(){
+            this.query= '' //this 는 vue인스턴스를 의미
+            //todo 검색결과를 숨기는 ...
+            debugger
+        }
+        
     }
 })
